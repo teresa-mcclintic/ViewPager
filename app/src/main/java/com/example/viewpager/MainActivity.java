f79f235bcb4124f569347fce3e2798d280b2dae1
Houman Sarabostani <houms@amazon.com>
2020-11-09 18:06:29 -0800

    View Pager Layout

---
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
